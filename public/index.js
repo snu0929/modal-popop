6ffec455da52218ac8a30aaca5b841405071c219
sonu kumar <snu0929@gmail.com>
2024-10-27 14:17:12 +0530

    Initial commit

---
+++ public/index.js
@@ -0,0 +1,96 @@
+const speakers = [
+  {
+    name: "John Doe",
+    profession: "Cheif Markeing Officer",
+    company: "Acme Corp",
+    image: "assets/Ellipse 24.svg", // Path to speaker image
+    description:
+      "Monotonectally synergize business communities rather than client-centric convergence. Assertively unleash cross-platform best practices rather than pandemic total linkage. Synergistically monetize parallel infomediaries whereas 2.0 mindshare. Dramatically pursue real-time markets through e-business strategic theme areas.",
+  },
+  {
+    name: "John Doe",
+    profession: "Cheif Engagment Officer",
+    company: "Acquia",
+    image: "assets/Ellipse 24 (1).svg",
+    description:
+      "A Chief Engagement Officer (CEO) is a senior executive responsible for fostering and maintaining strong relationships with key stakeholders, both internal and external to an organization. This role is crucial in building and strengthening brand reputation, driving customer loyalty, and improving employee morale.",
+  },
+  {
+    name: "John Doe",
+    profession: "Cheif Technical Developer",
+    company: "Pantheon",
+    image: "assets/Ellipse 24 (2).svg",
+    description:
+      "A Chief Technical Officer (CTO) is a senior executive responsible for overseeing an organization's technological operations. This role involves leading the technical team, setting the technological vision, and ensuring the smooth functioning of all technical systems.",
+  },
+  {
+    name: "John Doe",
+    profession: "Cheif Markeing Officer",
+    company: "Spacbee",
+    image: "assets/Ellipse 24 (3).svg",
+    description:
+      "A Chief Technology Officer (CTO) is a senior executive responsible for overseeing an organization's technological operations. This role involves leading the technical team, setting the technological vision, and ensuring the smooth functioning of all technical systems..",
+  },
+];
+
+function generateCards() {
+  const cardsContainer = document.getElementById("cards-container");
+  speakers.forEach((item, index) => {
+    const card = document.createElement("div");
+    card.className = "speaker-card";
+    card.innerHTML = `
+     <img src="${item.image}" alt="${item.name}" class="speaker-card__image"/>
+     <h3 class="speaker-card__name">${item.name}</h3>
+     <p class="speaker-card__profession">${item.profession}</p>
+     <p class="speaker-card__company">${item.company}</p>
+    `;
+    card.addEventListener("click", () => showDetails(item));
+    cardsContainer.appendChild(card);
+  });
+}
+
+function showDetails(item) {
+  const detailsSection = document.getElementById("speaker-details");
+  detailsSection.innerHTML = `
+  <div class="speaker-details__content">
+   <button class="close-button" onclick="closeDetails()">
+      <img src="assets/Vector (3).svg" alt="Close Icon"/>
+    </button>
+  <div class="speaker-details__left">
+    <div class="speaker-details__image-container">
+      <img src="${item.image}" alt="${item.name}" />
+    </div>
+
+    <div class="speaker-details__info-container">
+      <h2 class="speaker-details__name">${item.name}</h2>
+      <p class="speaker-details__profession">${item.profession}</p>
+      <p class="speaker-details__company">${item.company}</p>
+    
+      <div class="speaker-details__social-icons">
+        <img src="assets/Vector (4).svg" alt="Dribbble Icon"/>
+       
+        <img src="assets/Vector (5).svg" alt="twitter Icon"/>
+        <img src="assets/Vector (6).svg" alt="linkdin Icon"/>
+      </div>
+    </div>
+   
+   </div>
+
+   <div class="speaker-details__right">
+   <div class="speaker-details__description-container">
+      <p>${item.description}</p>
+    </div>
+ </div>
+  </div>
+
+
+`;
+  detailsSection.classList.remove("hidden");
+}
+
+function closeDetails() {
+  console.log("closeDetails");
+  document.getElementById("speaker-details").classList.add("hidden");
+}
+
+generateCards();
